544c277a26dcb7e94d8ac9246de0ec9cc5073fc1
shaheen praveen <shaheenpraveenpraveen183@gmail.com>
2024-12-31 11:26:18 +0530

    comparsion done

---
+++ Typescript/ModuleBasics/comparsion/helloworld.ts
@@ -0,0 +1,13 @@
+console.log("Hello World");
+//Variables
+//var 
+//let 
+//const
+var myDetail = {name:"Deepak"};
+var firstName = "Deepak";
+console.log(typeof firstName);
+firstName=10;
+console.log(myDetail.name);
+//console.log(myDetail.firstName); not possible in typescript, compile time error 
+var sum = 10 + "10";
+console.log(sum);
