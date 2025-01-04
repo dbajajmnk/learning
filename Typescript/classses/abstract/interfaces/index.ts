980484f24546d3acfbef981c15cca2e99f324810
shaheen praveen <shaheenpraveenpraveen183@gmail.com>
2025-01-04 10:12:42 +0530

    comment code

---
+++ Typescript/classses/abstract/interfaces/index.ts
@@ -0,0 +1,24 @@
+/*********************************** Theory ****************************************************
+ ** What we cover 
+ * Static Properties : What are the staic properties of a class along with real work examples of static properties already use in JavaScript
+ * abastract classes : Everything about abstract classes and how are they different from normal classes along with examples of inheritance and their use with child classes
+ * * All about Interfaces
+ * introduction : An Interface is a Binding Contract Between Two or More Components of an Application.
+ *   Example USB interface
+ *           User interfce
+ * Create interface 
+ * multiple inheritance
+ * generics with interfaces
+ * multiple types as generics
+ * classes & Interfaces
+ * access modifiers with interfaces
+*/
+
+/*********************************** Theory End *************************************************/
+/********************************** Practical  *************************************************/
+
+
+
+/***  */
+/*************************************Practial End ********************************************/
+/****************************** Practice  *****************************************************/
