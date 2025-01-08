b5a6dcf9e6878a2bd78d591c46a84eac9a24a024
shaheen praveen <shaheenpraveenpraveen183@gmail.com>
2025-01-08 19:22:53 +0530

    working on objects

--- Typescript/objectsintypescript/index.js
+++ Typescript/objectsintypescript/index.js
@@ -7,87 +7,49 @@
 /* Type Aliase for Objects : give name to type
 
 /* Nested Objects : Object inside object
-/* index signautes with Objects : similer to arrow function 
-/* optional properties : as it's name suggest these are optional not required 
+/* index signautes with Objects : similer to arrow function
+/* optional properties : as it's name suggest these are optional not required
 /* ReadOnly : once assigned can't be changed
 /* Union Types With Objects : How to union types with object
-/* Discriminating Unions : All the union types have common properties and based we can differenciate and work 
+/* Discriminating Unions : All the union types have common properties and based we can differenciate and work
 /* intersecion Types : similer to union but club all the types
-/* 
+/*
 
 /**************************************   Theory End  *************************************************************/
 /**************************************   Practical  **************************************************************/
-
 /* Intoduction to Objects : object properties and methods.*/
 //Create Object : Using object Literal
 //Using Object : 
-
 // let user= {
 //    name:"Deepak",experience:15,subjects:["Python","JavaScript"]
 // }
-
-let user:User= {
-   name:"Deepak",experience:15,subjects:["Python","JavaScript"]
-}
-
-
-
-let user1:Object= {
-   name:"Deepak",experience:15,subjects:["Python","JavaScript"]
-}
-let user2:Object= {
-   name:"Shivani",experience:1,subjects:["Figma","JavaScript"]
-
-}
-//Type alias
-type User = {
-   name:string,experience:number,subjects:string[]
-}
-
-   
-
+// let user:{}= {
+//    name:"Deepak",experience:15,subjects:["Python","JavaScript"]
+// }
+var user = {
+    name: "Deepak", experience: 15, subjects: ["Python", "JavaScript"]
+};
 console.log(user);
-
-
-
 // //  let deepak:Expert = {name:"Deepak",experience:15,subjects:["Python","JavaScript"]}
 // //  let sonika:Expert = {name:"Sonika",experience:2,subjects:["Python","JavaScript"]}
 // //  let kapil:Expert = {name:"Sonika",experience:2,subjects:["Python","JavaScript"]}
-
 // // type Address ={houseNo:string, street:string,city:string,state:string, country:string,pin:number}
 // // type Subject = {name:string,level:string}
-
 // // type Expert = {name:string,experience:number, subjects?:Subject[],address?:Address}
-
 //  let deepak:Expert= {name:"Deepak",experience:15,subjects:[{name:"Python",level:"JavaScript"}],address:{houseNo:"M-1401", street:"Abc", city:"Moonak",state:"Haryana",country:"India",pin:148033}}
 //  let sonika:Expert = {name:"Deepak",experience:15,subjects:[{name:"Python",level:"JavaScript"}]}
 //  let kapil:Expert={name:"Deepak",experience:15}
-
-
-
- 
-
-
-
 /* Working with Objects   : practical
    Type Aliase for Objects : give name to type
     Nested Objects : Object inside object
-    index signautes with Objects : similer to arrow function 
-    optional properties : as it's name suggest these are optional not required 
+    index signautes with Objects : similer to arrow function
+    optional properties : as it's name suggest these are optional not required
      ReadOnly : once assigned can't be changed
     Union Types With Objects : How to union types with object
-    Discriminating Unions : All the union types have common properties and based we can differenciate and work 
+    Discriminating Unions : All the union types have common properties and based we can differenciate and work
    intersecion Types : similer to union but club all the types*/
-
-
 /**************************************** Practical End *************************************************************/
 /**************************************** Practice   ***************************************************************/
 //How to create object
-
-
 /****************************************** Practice  **************************************************************/
-
-
-
-
-/************************************** Object in  Typescript End *******************************************/
+/************************************** Object in  Typescript End *******************************************/ 
