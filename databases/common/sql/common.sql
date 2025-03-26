9bec1afa1f99ed0a0881a3cce8134831ef2e8629
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2025-03-26 08:10:03 +0530

    update some use cases

--- databases/common/sql/common.sql
+++ databases/common/sql/common.sql
@@ -25,6 +25,15 @@ NOT
 --SELECT * FROM film WHERE rental_duration=7 AND rental_rate=4.99 AND film.length =117
 --SELECT * FROM film WHERE rental_duration=7 OR rental_rate=4.99 OR film.length =117
 ---SELECT * FROM film WHERE NOT rental_duration=7 
+
+/*****************Functions**********************
+Round()
+SUM()
+MIN()
+MAX()
+COUNT()
+AVG()
+**/
 /************** Aggregiation Functions **********/
 --SELECT COUNT(*) FROM film
 ---SELECT SUM(rental_duration) from film 
@@ -32,13 +41,62 @@ NOT
 ---SELECT MIN(rental_duration) from film 
 --SELECT MAX(rental_duration) from film 
 /******************************** Aggregation Function **********************/
-/*** Key Words *************/
+/*** Key Words **********
 ORDER BY - ASC, DESC
 AS
-GROUP BY
-LIKE
+GROUP BY : 
+GROUP BY clause is used with the SELECT statement.
+In the query, the GROUP BY clause is placed after the WHERE clause.
+In the query, the GROUP BY clause is placed before the ORDER BY clause if used.
+In the query, the Group BY clause is placed before the Having clause.
+Place condition in the having clause.
+DISTINCT
 BETWEEN
 IN
+LIKE 
+ILIKE
+SELECT 
+FROM
+HAVING : 
+The HAVING clause is an essential tool in SQL for filtering results based on aggregated data. 
+Unlike the WHERE clause, which applies conditions to individual rows, 
+HAVING works on groups of data that have been aggregated using functions like SUM(), AVG(), and COUNT(). 
+Understanding how and when to use the HAVING clause allows you to perform more complex data analysis and generate meaningful insights from your datasets
+Difference 
+Having
+In the HAVING clause it will check the condition in group of a row.	
+HAVING clause can only be used with aggregate function.	
+Priority Wise HAVING Clause is executed after Group By.	
+
+
+WHERE
+In the WHERE condition it will check or execute at each row individual.
+The WHERE Clause cannot be used with aggregate function like Having
+Priority Wise WHERE is executed before  Group By.
+
+JOIN
+***/
+--SELECT title from film ORDER BY title
+/**** Pattern Matching Characters ***
+_ single charactor
+% - Occurance of the character**/
+SELECT 
+    title,
+    length,
+    CASE 
+        WHEN length < 60 THEN 'Short'
+        WHEN length BETWEEN 60 AND 120 THEN 'Medium'
+        ELSE 'Long'
+    END AS length_category
+FROM film;
+
+
+SELECT 
+    customer_id,
+    COUNT(rental_id) AS rental_count
+FROM rental
+GROUP BY customer_id
+ORDER BY rental_count DESC;
 
 
 
