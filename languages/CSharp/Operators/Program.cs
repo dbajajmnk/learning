b0ba957b38595ad07ae6a91a497b827754b7b8fb
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2025-05-04 17:16:46 +0530

    Opeartor in Progress

---
+++ languages/CSharp/Operators/Program.cs
@@ -0,0 +1,13 @@
+﻿//Arthmatic Operators
+int a=20;
+int b = 10;
+int c=a+b;
+c=a*b;
+c=a/b;
+//Logical Operator
+//Assignment Operator
+//Relational Operator
+//null-coalescing Operator
+//??  check value use if it's not null if null use right hand side value
+//??= if(value==null) { value="Something"}
+
