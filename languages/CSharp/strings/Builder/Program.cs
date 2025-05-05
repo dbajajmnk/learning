819360b60d8ccf359f08a3431ccc4edc8a3d5cc4
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2025-05-05 08:22:02 +0530

    builder comments done

---
+++ languages/CSharp/strings/Builder/Program.cs
@@ -0,0 +1,14 @@
+﻿/**
+Mutuable 
+In Cosntructor you can pass intial Value and Capacity
+and print the capacity and lenth as well
+you can use append 
+to add new value into last of string
+Append
+AppendLine
+AppendFormat
+AppendJoin
+Replace
+Insert string into given 
+toString
+**/
