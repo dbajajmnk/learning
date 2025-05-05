c73f31b9126d92374d068ad628fe6034b07385d0
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2025-05-05 06:46:09 +0530

    StringOpertionsAdded

---
+++ languages/CSharp/strings/Operatiosn/Program.cs
@@ -0,0 +1,24 @@
+﻿//Length is proprty in string object not a method
+//you can access charactre like name[0]
+//with the help of foreach you can print the character of string
+//Concat Method
+string strConcat = "";
+string[] data = {"one","two","Three"};
+
+strConcat = String.Concat(data);
+Console.WriteLine(strConcat);
+//Join
+//Comparsion
+/*
+0 Matched
+<0
+>0
+*/
+//Equal return true and false
+//Searching
+/*
+indexOf
+lastIndexOf
+replace
+
+*/
